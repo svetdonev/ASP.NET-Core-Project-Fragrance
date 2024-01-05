11743d308ca1674b535cfb44b06b02f0f5b952ac
Svetlozar Donev <svetdonev1@gmail.com>
2024-01-05 02:37:17 +0200

    Search field form

--- FragranceProject/Models/Fragrances/AllFragrancesQueryModel.cs
+++ FragranceProject/Models/Fragrances/AllFragrancesQueryModel.cs
@@ -5,7 +5,7 @@ namespace FragranceProject.Models.Fragrances
     public class AllFragrancesQueryModel
     {
         public IEnumerable<string> Names { get; set; }
-        public IEnumerable<string> SearchTerm { get; set; }
+        public string SearchTerm { get; set; }
         public FragranceSorting Sorting { get; set; }
         public IEnumerable<FragranceListingViewModel> Fragrances { get; set; }
     }
