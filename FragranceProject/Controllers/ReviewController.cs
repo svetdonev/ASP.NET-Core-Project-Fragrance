870dad4e40ffd4ca1e1febf5962247f78b7f18f6
Svetlozar Donev <svetdonev1@gmail.com>
2024-01-25 23:56:28 +0200

    Review controller added

---
+++ FragranceProject/Controllers/ReviewController.cs
@@ -0,0 +1,13 @@
+﻿using FragranceProject.Models.Fragrances;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FragranceProject.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Create(FragranceListingViewModel fragrance)
+        {
+            return View();
+        }
+    }
+}
